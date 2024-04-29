9099c44dfa0c1a3ae8dbc41d317a0275dcb42867
HenriqueFerC <rm552325@fiap.com.br>
2024-04-29 13:01:19 -0300

    Subindo estrutura API

---
+++ src/main/java/br/com/fiap/CheckPoint1/dto/cliente/AtualizarClienteFuncDto.java
@@ -0,0 +1,6 @@
+package br.com.fiap.CheckPoint1.dto.cliente;
+
+import br.com.fiap.CheckPoint1.model.Funcionario;
+
+public record AtualizarClienteFuncDto(Funcionario funcionario) {
+}
---
+++ src/main/java/br/com/fiap/CheckPoint1/dto/funcionario/AtualizarFuncClienteDto.java
@@ -0,0 +1,6 @@
+package br.com.fiap.CheckPoint1.dto.funcionario;
+
+import br.com.fiap.CheckPoint1.model.Cliente;
+
+public record AtualizarFuncClienteDto(Cliente cliente) {
+}
